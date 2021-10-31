9ea12b3843adacfff1c0ad9469f5e97c53d62872
SimchenkoDmitry <dsimchenko@inbox.ru>
2021-10-31 20:02:35 +0300

    Спасибо за комментарий по предыдущему заданию. Разобрался. Все заработало

---
+++ Урок 6/task 6.5.py
@@ -0,0 +1,43 @@
+"""5.	Реализовать класс Stationery (канцелярская принадлежность).
+
+●	определить в нём атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение «Запуск отрисовки»;
+●	создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
+●	в каждом классе реализовать переопределение метода draw.
+Для каждого класса метод должен выводить уникальное сообщение;
+●	создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+"""
+
+
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('запуск отрисовки')
+
+
+class Pen(Stationary):
+    def draw(self):
+        super().draw()
+        print(f"Я рисую {self.title}")
+
+
+class Pencil(Stationary):
+    def draw(self):
+        super().draw()
+        print(f"Я рисую {self.title}")
+
+
+class Handle(Stationary):
+    def draw(self):
+        super().draw()
+        print(f"Я рисую {self.title}")
+
+
+pen = Pen("pen")
+pencil = Pencil("pencil")
+handle = Handle("handle")
+pen.draw()
+pencil.draw()
+handle.draw()
